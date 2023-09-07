c06078554afab716b1b5e6c5863b828af5ecbb12
Kyle LeMaster <kyle.lemaster@gmail.com>
2023-09-07 12:59:46 -0500

    Hide Menu

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function createWindow() {
 
     // Hide Menu Bar - Alt to Show
     mainWindow.autoHideMenuBar = true;
-    // mainWindow.setMenu(null);
+    mainWindow.setMenu(null);
 
     mainWindow.on('closed', function () {
         mainWindow = null;
@@ -62,7 +62,7 @@ function checkForUpdates() {
 function getLocalVersion() {
     // Retrieve local version from a file or local setting
     // This is a mock; replace with your method of tracking local version
-    return "0.7.17";  
+    return "0.7.17";
 }
 
 function downloadAndExtractGame() {
